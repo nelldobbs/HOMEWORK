1634680ddafe1253c78a5eb38bf411a3fb3dd5ab
nelldobbs <31457899+nelldobbs@users.noreply.github.com>
2017-10-01 11:03:38 -0400

    Add files via upload

--- wk5hw/sketch.js
+++ wk5hw/sketch.js
@@ -1,5 +1,14 @@
+var circX;
+var circY;
+var circDiameter = 450;
+var circRadius = circDiameter / 2;
+var d;
+var redCol = 200;
+var img;
 var antonFont; 
 var img;
+var isClicked = false;
+
 function preload(){
     
   antonFont = loadFont("Anton.ttf");  
@@ -8,7 +17,8 @@ function preload(){
 }
  function setup (){
     createCanvas(windowWidth, windowHeight); 
-//     textFont("antonFont");
+circX = windowWidth / 2;
+    circY = windowHeight / 2;
      textFont(antonFont);
      textSize(60);
    
@@ -16,13 +26,39 @@ function preload(){
 
 function draw(){
   
-   background(100,200,260);
-   text("Ooowee, now that's a good looking burger!", mouseX, mouseY);
-
-    imageMode(CENTER);
-    image(img, width/2, height/2, 600, 300);
-    
+   background(redCol,10,260);
+    fill (105);
+    noStroke();
+    ellipse(circX, circY, circDiameter, circDiameter);
+    d = dist(circX, circY, mouseX, mouseY);
+    if (d < circRadius) {
+        redCol = 0;
+        fill(100, 150, 200);
+        text("MMMMMMmmm, that was a good burger.", mouseX, mouseY, 300, 800);
+    } else {
+        redCol = 200;
+           imageMode(CENTER);
+    image(img, width/2, height/2, 200, 200);
+            text("Ooowee, now that's a good looking burger!", mouseX, mouseY, 300, 800);
     }
+    
+}
+
+function mousePressed() {
+    if (d < circRadius) {
+        redCol = 0;
+    } else {
+        redCol = 200;
+    }
+    
+
+
+}
+
+    
+    
+    
+ 
 
 
         
