7e1bb518f0fd0286707278701f1d71c013b5d9f4
nelldobbs <31457899+nelldobbs@users.noreply.github.com>
2017-10-13 18:00:10 -0400

    Add files via upload

---
+++ wk1_ homework/sketch.js
@@ -0,0 +1,21 @@
+function setup(){
+createCanvas(1024,768);    
+    
+    
+    
+}
+function draw() {
+ background(200,103,40);
+  noStroke();
+
+ ellipse(512,400,300,300); 
+ 
+    fill(150, 50, 200);
+rect(380, 400, 95, 20)
+    
+noStroke();
+    ellipse(600, 390, 50, 50);
+   fill(100, 200, 182);
+    
+    
+}
